baa688b37c02c97690d475956055026635042eca
Lex Tarasov <l3xtarasov@yandex.ru>
2025-10-26 21:08:29 +0300

    Create method delete and put

--- src/main/java/org/example/springweb/User.java
+++ src/main/java/org/example/springweb/User.java
@@ -1,4 +1,14 @@
 package org.example.springweb;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private int id;
+    private String name;
+    private int age;
 }
--- src/main/java/org/example/springweb/UserController.java
+++ src/main/java/org/example/springweb/UserController.java
@@ -1,4 +1,55 @@
 package org.example.springweb;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+   private List<User> users = new ArrayList<>();
+
+
+   @PostMapping
+   public  ResponseEntity<User>  addUser(@RequestBody User user) {
+       users.add(user);
+       return ResponseEntity.status(HttpStatus.CREATED).body(user);
+
+   }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> user(@PathVariable int id) {
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getId() == id){
+                return ResponseEntity.status(200).body(users.get(i));
+            }
+        }
+        return ResponseEntity.status(404).body(null);
+    }
+
+
+    @DeleteMapping
+    public ResponseEntity<User> deleteUser(@RequestBody User user) {
+       users.remove(user);
+       return ResponseEntity.status(200).body(user);
+    }
+
+
+    @PutMapping
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+       for (int i = 0; i < users.size(); i++) {
+           if (users.get(i).getId() == user.getId()){
+               users.set(i, user);
+           }
+       }
+       return ResponseEntity.status(200).body(user);
+    }
+
+    // До реализовать методы удаления юзера изменения и замены и получения всех юзеров
+
 }
